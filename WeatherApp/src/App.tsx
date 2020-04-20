e81786bb2dff22d0e89602d36894fca5445643bc
Data Analytics3 Cell 송석민 <seokmin92@sk.com>
2020-04-21 01:44:10 +0900

    Chapter 7 초기화

---
+++ WeatherApp/src/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React,{Fragment} from 'react';
+import Styled from 'styled-components/native';
+
+const Container = Styled.View`
+  flex:1;
+  background-color:#EEE;
+`;
+
+const App = () => {
+  return (
+      <Container>
+      </Container>
+  );
+};
+
+export default App;
